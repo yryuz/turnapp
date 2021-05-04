e0a6aca474f73f0c936e33a294175e3a092e362e
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-04 15:53:38 +0900

    投稿に関わるモデル、テーブル作成

---
+++ app/controllers/tunes_controller.rb
@@ -0,0 +1,20 @@
+class TunesController < ApplicationController
+
+  def index
+    @tunes = Tune.all
+  end
+
+  def new
+    @tune = Tune.new
+  end
+
+  def create
+    Tune.create(tune_params)
+  end
+
+  private
+  def tune_params
+    params.require(:tune).permit(:url, :name, :artist, :genre_id, :text, :user, :image)
+  end
+  
+end
