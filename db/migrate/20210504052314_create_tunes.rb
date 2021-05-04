79ab1f8ec0360b08c1f7d3b19c1596fa2769ba4a
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-04 15:25:19 +0900

    first commit

---
+++ db/migrate/20210504052314_create_tunes.rb
@@ -0,0 +1,13 @@
+class CreateTunes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tunes do |t|
+      t.string :url, null: false
+      t.string :name, null: false
+      t.string :artist, null: false
+      t.integer :genre_id, null: false
+      t.text :text, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
