e0a6cc0ae9b4931c66aec3efab4d287a1fc6e041
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-04 16:04:01 +0900

    Active Storageの導入

--- app/controllers/tunes_controller.rb
+++ app/controllers/tunes_controller.rb
@@ -14,7 +14,7 @@ class TunesController < ApplicationController
 
   private
   def tune_params
-    params.require(:tune).permit(:url, :name, :artist, :genre_id, :text, :user, :image)
+    params.require(:tune).permit(:url, :name, :artist, :genre_id, :text, :user, :image).merge(user_id: current_user.id)
   end
-  
+
 end
