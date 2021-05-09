759aa92c6a0d319d51f37d005add1e2a0186602b
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-09 23:34:35 +0900

    ユーザー情報編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
+  
+
+
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @tunes = user.tunes
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @tunes = @user.tunes
   end
-end
+
+  def edit
+    @user = User.find(params[:id])
+    if user_signed_in? && current_user.id == @user.id
+      render "edit"
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :year, :introduction)
+  end
+
+
+  end
+
