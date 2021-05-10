60393ee6bb0f1d602c277060dfa2eb8d68ec4373
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-10 15:59:12 +0900

    パス変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
-    redirect_to root_path
+    @comment = Comment.create(comment_params)
+    redirect_to tune_path(@comment.tune.id)
   end
 
   private
