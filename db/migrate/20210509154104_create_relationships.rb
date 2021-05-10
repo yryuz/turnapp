95db5e8faa30c886c7d4520fb033852e44a24636
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-11 00:40:32 +0900

    マイグレーションファイル編集

--- db/migrate/20210509154104_create_relationships.rb
+++ db/migrate/20210509154104_create_relationships.rb
@@ -1,7 +1,7 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.references :user
+      t.references :user, foreign_key: true
       t.references :follower, foreign_key: { to_table: :users }
       t.timestamps
       t.index [:user_id, :follower_id], unique: true
