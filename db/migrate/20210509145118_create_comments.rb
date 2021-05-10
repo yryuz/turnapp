a9485cb5ff07a2860044ed530665e7c08fbe8f8b
yryuz <81128880+yryuz@users.noreply.github.com>
2021-05-11 01:37:25 +0900

    マイグレーションファイル編集

--- db/migrate/20210509145118_create_comments.rb
+++ db/migrate/20210509145118_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :tune_id
+      t.integer :user_id, foreign_key: true
+      t.integer :tune_id, foreign_key: true
       t.text :text
       t.timestamps
     end
